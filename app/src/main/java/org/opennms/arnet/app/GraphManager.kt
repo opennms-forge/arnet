38f6ddfe16b9aabedeb13759bef2d68ff6d83309
Jesse White <jesse@opennms.org>
2019-12-16 16:52:26 -0500

    Make the graph change over time.

--- app/src/main/java/org/opennms/arnet/app/GraphManager.kt
+++ app/src/main/java/org/opennms/arnet/app/GraphManager.kt
@@ -1,12 +1,53 @@
 package org.opennms.arnet.app
 
 import android.content.Context
+import com.google.ar.core.Frame
+import com.google.ar.sceneform.FrameTime
 import com.google.ar.sceneform.Scene
 import edu.uci.ics.jung.graph.Graph
 import org.opennms.arnet.api.Consumer
 import org.opennms.arnet.api.model.*
+import java.util.concurrent.atomic.AtomicBoolean
 
-class GraphManager(private val context: Context,private val scene: Scene) : Consumer {
+class GraphManager(private val context: Context,private val scene: Scene) : Consumer, Scene.OnUpdateListener {
+    var didGraphChange: AtomicBoolean = AtomicBoolean(false)
+    lateinit var graphNode : GraphNode
+
+
+    init {
+        scene.addOnUpdateListener(this);
+    }
+
+    /**
+     * Called when the frame for the scene has been updated.
+     */
+    override fun onUpdate(frameTime: FrameTime?) {
+       if (didGraphChange.get()) {
+           didGraphChange.set(false);
+
+           graphNode.render(scene);
+       }
+    }
+
+    override fun accept(
+        graph: Graph<Vertex, Edge>?,
+        alarms: MutableCollection<Alarm>?,
+        situations: MutableCollection<Situation>?
+    ) {
+        if (!::graphNode.isInitialized) {
+            graphNode = GraphNode(context, graph);
+            graphNode.render(scene);
+            scene.addChild(this.graphNode);
+        }
+    }
+
+    override fun acceptEdge(e: Edge?) {
+        didGraphChange.set(true)
+    }
+
+    override fun acceptVertex(v: Vertex?) {
+        didGraphChange.set(true)
+    }
 
     override fun acceptDeletedAlarm(reductionKey: String?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -28,24 +69,6 @@ class GraphManager(private val context: Context,private val scene: Scene) : Cons
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun accept(
-        graph: Graph<Vertex, Edge>?,
-        alarms: MutableCollection<Alarm>?,
-        situations: MutableCollection<Situation>?
-    ) {
-        val node = GraphNode(context, graph);
-        node.render(scene)
-        scene.addChild(node)
-    }
-
-    override fun acceptEdge(e: Edge?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun acceptVertex(v: Vertex?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
     override fun acceptDeletedVertex(vertexId: String?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
@@ -53,5 +76,6 @@ class GraphManager(private val context: Context,private val scene: Scene) : Cons
     override fun acceptAlarm(a: Alarm?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
 
 }
--- app/src/main/java/org/opennms/arnet/app/mock/MockConsumerService.java
+++ app/src/main/java/org/opennms/arnet/app/mock/MockConsumerService.java
@@ -6,6 +6,12 @@ import org.opennms.arnet.api.model.Edge;
 import org.opennms.arnet.api.model.Vertex;
 
 import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.SparseMultigraph;
@@ -17,17 +23,56 @@ public class MockConsumerService implements ConsumerService {
 
     private final Graph<Vertex, Edge> g = new SparseMultigraph<>();
 
-    public MockConsumerService() {
-        g.addVertex(MyVertex.forId(1));
-        g.addVertex(MyVertex.forId(2));
-        g.addVertex(MyVertex.forId(3));
+    private final AtomicReference<Vertex> lastVertex = new AtomicReference<>();
+    private final AtomicInteger nextId = new AtomicInteger(0);
 
-        g.addEdge(MyEdge.forId("Edge-A"), MyVertex.forId(1), MyVertex.forId(2));
-        g.addEdge(MyEdge.forId("Edge-B"), MyVertex.forId(2), MyVertex.forId(3));
+    private final Set<Consumer> consumers = new LinkedHashSet<>();
+
+    public MockConsumerService() {
+        // Create some fixed graph to start
+        Vertex v1 = MyVertex.forId(1);
+        g.addVertex(v1);
+        Vertex v2 = MyVertex.forId(2);
+        g.addVertex(v2);
+        Vertex v3 = MyVertex.forId(3);
+        g.addVertex(v3);
+        g.addEdge(MyEdge.forId("Edge-1-2"), v1, v2);
+        g.addEdge(MyEdge.forId("Edge-2-3"), v2, v3);
+
+        // Setup for generation
+        lastVertex.set(v3);
+        nextId.set(4);
+
+        // Every 1 sec, add another vertex and edge
+        Timer t = new Timer();
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // Generate the vertex and edge
+                MyVertex v = MyVertex.forId(nextId.getAndIncrement());
+                g.addVertex(v);
+                Edge e = MyEdge.forId(String.format("Edge-%s-%s", lastVertex.get().getId(), v.getId()));
+                g.addEdge(e, lastVertex.get(), v);
+
+                // Notify the consumers
+                synchronized (consumers) {
+                    for (Consumer consumer : consumers) {
+                        consumer.acceptVertex(v);
+                        consumer.acceptEdge(e);
+                    }
+                }
+
+                // Ready for next iteration
+                lastVertex.set(v);
+            }
+        }, 1000, 1000);
     }
 
     @Override
     public void accept(Consumer consumer) {
         consumer.accept(g, Collections.emptyList(), Collections.emptyList());
+        synchronized (consumers) {
+            consumers.add(consumer);
+        }
     }
 }
