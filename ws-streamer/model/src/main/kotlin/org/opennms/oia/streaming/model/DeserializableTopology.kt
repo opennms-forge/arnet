bbda12f61c0815ba8f180dcd67a0f5d860d9c2a4
Matthew Brooks <matt@opennms.com>
2019-12-19 14:29:15 -0500

    Ooops....

--- ws-streamer/model/src/main/kotlin/org/opennms/oia/streaming/model/DeserializableTopology.kt
+++ ws-streamer/model/src/main/kotlin/org/opennms/oia/streaming/model/DeserializableTopology.kt
@@ -2,12 +2,11 @@ package org.opennms.oia.streaming.model
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import org.opennms.integration.api.v1.model.Alarm
-import org.opennms.integration.api.v1.model.Node
 import org.opennms.integration.api.v1.model.TopologyEdge
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-data class Topology(
-    val nodes: Set<Node>? = null,
+data class DeserializableTopology(
+    val nodes: Set<DeserializableNode>? = null,
     val edges: Set<TopologyEdge>? = null,
     val alarms: Set<Alarm>? = null
 )
