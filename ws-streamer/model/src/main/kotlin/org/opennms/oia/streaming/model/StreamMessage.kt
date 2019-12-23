647149b6077dbb2081dfb99db8ed06ab77b34bf7
Matthew Brooks <matt@opennms.com>
2019-12-23 16:42:44 -0500

    Fix for deserializing alarm delete

--- ws-streamer/model/src/main/kotlin/org/opennms/oia/streaming/model/StreamMessage.kt
+++ ws-streamer/model/src/main/kotlin/org/opennms/oia/streaming/model/StreamMessage.kt
@@ -1,6 +1,8 @@
 package org.opennms.oia.streaming.model
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.convertValue
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.opennms.integration.api.deserializer.oiaDeserializer
 import org.opennms.integration.api.v1.model.Alarm
 import org.opennms.integration.api.v1.model.InMemoryEvent
@@ -31,6 +33,10 @@ fun eventMessage(event: InMemoryEvent) = StreamMessage(MessageType.Event, event)
 fun topologyMessage(topology: Topology) = StreamMessage(MessageType.Topology, topology)
 fun nodeMessage(node: Node) = StreamMessage(MessageType.Node, node)
 
-inline fun <reified T> StreamMessage.deserializePayload(): T {
-    return oiaDeserializer.convertValue(this.payload)
-}
+val mapper: ObjectMapper by lazy { jacksonObjectMapper() }
+
+inline fun <reified T> StreamMessage.deserializePayload(): T =
+    when (this.type) {
+        MessageType.AlarmDelete -> mapper.convertValue(this.payload)
+        else -> oiaDeserializer.convertValue(this.payload)
+    }
