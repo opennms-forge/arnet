839e582d18b37a2edcba423c3d10a7600d0d14bb
qauseen <qauseen@opennms.ca>
2020-01-01 14:46:28 -0500

    ActionBar & View Rotation (draft)

--- app/src/main/java/org/opennms/arnet/app/MainActivity.kt
+++ app/src/main/java/org/opennms/arnet/app/MainActivity.kt
@@ -1,11 +1,10 @@
 package org.opennms.arnet.app
 
 import android.os.Bundle
+import android.util.AttributeSet
 import android.util.Log
-import android.view.GestureDetector
-import android.view.MotionEvent
-import android.view.ScaleGestureDetector
-import android.view.View
+import android.view.*
+import android.view.MotionEvent.INVALID_POINTER_ID
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.ar.core.AugmentedImage
@@ -23,6 +22,9 @@ import org.opennms.arnet.app.scene.RenderableRegistry
 import java.util.concurrent.atomic.AtomicBoolean
 import kotlin.math.max
 import kotlin.math.min
+import androidx.core.view.MotionEventCompat
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -41,6 +43,17 @@ class MainActivity : AppCompatActivity() {
     private val applyTransforms = AtomicBoolean(false)
     private var scaleFactor: Float = 1.0f
 
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.drawer_menu, menu)
+        val actionBar = supportActionBar
+        actionBar?.title = "SOMMMMMEEE TITLE"
+        actionBar?.setDisplayShowTitleEnabled(true)
+        Log.e("CHEEEEECKKKKKED::::", "checked")
+        return true;
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -56,6 +69,7 @@ class MainActivity : AppCompatActivity() {
                 // Don't let the object get too small or too large
                 scaleFactor = max(0.1f, min(scaleFactor, 5.0f))
                 applyTransforms.set(true)
+
                 return true;
             }
         })
@@ -68,10 +82,82 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+
+
         arFragment.getArSceneView().setOnTouchListener(object : View.OnTouchListener {
-            override fun onTouch(v: View?, ev: MotionEvent?): Boolean {
-                simpleGestureDetector.onTouchEvent(ev)
+            private var mLastTouchX: Float = 0.0f
+            private var mLastTouchY: Float = 0.0f
+            private var mPosX: Float = 0.0f
+            private var mPosY: Float = 0.0f
+
+//            override fun onTouch(v: View?, ev: MotionEvent?): Boolean {
+//                simpleGestureDetector.onTouchEvent(ev)
+//                scaleGestureDetector.onTouchEvent(ev)
+//
+//
+//
+//                return true
+//            }
+
+            // The ‘active pointer’ is the one currently moving our object.
+            private var mActivePointerId = INVALID_POINTER_ID
+
+            override fun onTouch(v: View?, ev: MotionEvent?): Boolean  {
+                // Let the ScaleGestureDetector inspect all events.
                 scaleGestureDetector.onTouchEvent(ev)
+
+                val action = MotionEventCompat.getActionMasked(ev)
+
+                when (action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        MotionEventCompat.getActionIndex(ev).also { pointerIndex ->
+                            // Remember where we started (for dragging)
+                            mLastTouchX = MotionEventCompat.getX(ev, pointerIndex)
+                            mLastTouchY = MotionEventCompat.getY(ev, pointerIndex)
+                            Log.e("dowwn: ", "doyn")
+                        }
+
+                        // Save the ID of this pointer (for dragging)
+                        mActivePointerId = MotionEventCompat.getPointerId(ev, 0)
+                    }
+
+                    MotionEvent.ACTION_MOVE -> {
+                        // Find the index of the active pointer and fetch its position
+                        val (x: Float, y: Float) =
+                            MotionEventCompat.findPointerIndex(ev, mActivePointerId).let { pointerIndex ->
+                                // Calculate the distance moved
+                                MotionEventCompat.getX(ev, pointerIndex) to
+                                        MotionEventCompat.getY(ev, pointerIndex)
+                            }
+
+                        mPosX += x - mLastTouchX
+                        mPosY += y - mLastTouchY
+
+//                        invalidate()
+
+                        // Remember this touch position for the next move event
+                        mLastTouchX = x
+                        mLastTouchY = y
+                    }
+                    MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                        mActivePointerId = INVALID_POINTER_ID
+                    }
+                    MotionEvent.ACTION_POINTER_UP -> {
+
+                        MotionEventCompat.getActionIndex(ev).also { pointerIndex ->
+                            MotionEventCompat.getPointerId(ev, pointerIndex)
+                                .takeIf { it == mActivePointerId }
+                                ?.run {
+                                    // This was our active pointer going up. Choose a new
+                                    // active pointer and adjust accordingly.
+                                    val newPointerIndex = if (pointerIndex == 0) 1 else 0
+                                    mLastTouchX = MotionEventCompat.getX(ev, newPointerIndex)
+                                    mLastTouchY = MotionEventCompat.getY(ev, newPointerIndex)
+                                    mActivePointerId = MotionEventCompat.getPointerId(ev, newPointerIndex)
+                                }
+                        }
+                    }
+                }
                 return true
             }
         })
@@ -81,6 +167,13 @@ class MainActivity : AppCompatActivity() {
 
         // Start listening for tracked images
         arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdateFrame);
+
+        val actionBar = supportActionBar
+        actionBar!!.hide()
+
+        actionBar.show()
+        actionBar.subtitle = "subtitle"
+        actionBar.title = "title"
     }
 
     override fun onPause() {
@@ -151,6 +244,14 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+        val actionBar = supportActionBar
+        actionBar?.title = "SOMMMMMEEE TITLE"
+        actionBar?.setDisplayShowTitleEnabled(true)
+        supportActionBar?.show()
+        supportActionBar?.setDisplayShowCustomEnabled(true);
+        supportActionBar?.setDisplayShowHomeEnabled(true);
+        Log.e("CHEEEEECKKKKKED AGAIN 2::::", "checked")
     }
 
     companion object {
